ed0a969844545e62333ab5ec9b3739076b657bf8
Nitish Singh <nitishsingh925@gmail.com>
2023-10-08 22:10:46 +0530

    fix bug in contact form

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,18 +1,18 @@
-// Header Scroll 
+// Header Scroll
 let nav = document.querySelector(".navbar");
-window.onscroll = function(){
-    if(document.documentElement.scrollTop>50){
-        nav.classList.add("header-scrolled");
-    }else{
-        nav.classList.remove("header-scrolled");
-    }
-}
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 50) {
+    nav.classList.add("header-scrolled");
+  } else {
+    nav.classList.remove("header-scrolled");
+  }
+};
 
-//  nav hide 
+//  nav hide
 let navBar = document.querySelectorAll(".nav-link");
 let navCollapse = document.querySelector(".navbar-collapse.collapse");
-navBar.forEach(function(e){
-    e.addEventListener("click", function(){
-        navCollapse.classList.remove("show");
-    })
-})
+navBar.forEach(function (e) {
+  e.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
